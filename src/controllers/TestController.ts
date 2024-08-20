005881f7509f3235ea69d408aac7f84b92007ac5
Sam Dagnall <sam.dagnall@kainos.com>
2024-08-20 10:49:14 +0100

    Added search functionality on first page. Need to wait for CSV reading to work before pursuing further

--- src/controllers/TestController.ts
+++ src/controllers/TestController.ts
@@ -1,6 +1,24 @@
 import express from "express";
-import { getDatabases } from "../services/TestService"
+import { getDatabases, getLocations } from "../services/TestService"
 
 export const renderHomePage = async (req: express.Request, res: express.Response): Promise<void> => {
     res.render('home.html');
 }
+
+export const renderSearchPage = async (req: express.Request, res: express.Response): Promise<void> => {
+    res.render('search.html');
+}
+
+export const postSearchForm = async (req: express.Request, res: express.Response): Promise<void> => {
+    req.session.location = req.body.location;
+    res.redirect('/search-result');
+}
+
+export const renderSearchResultsPage = async (req: express.Request, res: express.Response): Promise<void> => {
+    const locationString = req.session.location;
+    const location = await getLocations(locationString, req.session.token);
+
+    res.render('search-result.html', {locations: location});
+
+}
+
